f3504f87cd411865d2d0742e120f4807d9ae9283
dishant1890 <dishantt32@gmail.com>
2026-01-15 12:13:49 +0530

    Update insertby.java

---
+++ INVENTORYPROJECTT/src/main/java/com/example/INVENTORYPROJECTT/insertby.java
@@ -0,0 +1,29 @@
+package com.example.INVENTORYPROJECTT;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class insertby implements CommandLineRunner {
+
+    private final userrepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public insertby(userrepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (userRepository.findByUsername("admin").isEmpty()) {
+            AppUser admin = new AppUser();
+            admin.setUsername("admin");
+            admin.setPassword(passwordEncoder.encode("admin123")); // <-- raw password
+            admin.setRole("ADMIN");
+            userRepository.save(admin);
+            System.out.println("Admin user created!");
+        }
+    }
+}
